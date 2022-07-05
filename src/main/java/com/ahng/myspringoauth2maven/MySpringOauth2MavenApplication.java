df13e1961a16308b1d608100603bf1ecc3abecb3
42bahn <bbu0704@gmail.com>
2022-07-05 14:40:37 +0900

    Logout - js-cookie, csrf

--- src/main/java/com/ahng/myspringoauth2maven/MySpringOauth2MavenApplication.java
+++ src/main/java/com/ahng/myspringoauth2maven/MySpringOauth2MavenApplication.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/ahng/myspringoauth2maven/config/SecurityConfiguration.java
+++ src/main/java/com/ahng/myspringoauth2maven/config/SecurityConfiguration.java
@@ -4,16 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@EnableWebSecurity
+// @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 	
@@ -33,11 +28,9 @@ public class SecurityConfiguration {
 			.antMatchers("/", "/error", "/webjars/**").permitAll()
 			.anyRequest().authenticated()
 		)
-			// .httpBasic(withDefaults())
-			.exceptionHandling((e) -> e.
-				authenticationEntryPoint(new 
-					HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-			)
+			.exceptionHandling((e) -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+			.csrf((c) -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+			.logout((l) -> l.logoutSuccessUrl("/").permitAll())
 			.oauth2Login();
 		return http.build();
 	}
