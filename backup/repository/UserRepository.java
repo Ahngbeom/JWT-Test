7ecf271db6a383e83c2a93967bcbad48d4c269f2
42bahn <bbu0704@gmail.com>
2022-07-10 14:56:25 +0900

    Delete backup Folder

--- backup/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ahng.myspringoauth2maven.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ahng.myspringoauth2maven.DTO.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
